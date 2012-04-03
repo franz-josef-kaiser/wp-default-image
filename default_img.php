75491821cf092b44750fe1d4bbce9007b48fda0a
franz-josef-kaiser <24-7@gmx.net>
2012-04-03 20:06:07 +0200

    shortened function - preparation for class and shortcode

--- default_img.php
+++ default_img.php
@@ -1,11 +1,10 @@
 <?php
-declare ( encoding = 'UTF-8' );
 /*
 Plugin Name: Default Image
 Plugin URI: http://unserkaiser.com
 Description: Adds a default image that can be used during development. Also crops on demand
 Author: Franz Josef Kaiser
-Version: 0.1
+Version: 0.3
 Author URI: http://unserkaiser.com
 License: MIT
 
@@ -33,14 +32,14 @@ if 4th argument set to true when registering the size using add_image_size();.
  */
 
 // Prevent loading this file directly - Busted!
-if( ! class_exists('WP') ) 
+if ( ! class_exists('WP') ) 
 {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit;
 }
 
-
+#################### @TODO Align with dynamic_image_resize class
 
 /** 
  * Default images for themes 
@@ -54,10 +53,10 @@ if( ! class_exists('WP') )
  *  
  * @param $args (array) 
  *              string $url URl to the given default image. 
- *              string $size Optional. Default is 'medium'. 
+ *              string $size Optional. Default 'medium'. 
  *              string (optional) $alt Image Description for the alt attribute. 
  *              string (optional) $title Image Description for the title attribute. 
- *              string (optional) $align Part of the class name for aligning the image. 
+ *              string (optional) $align Part of the class name for aligning the image. Default: 'none'
  *              string (optional) $echo Wheter to return or echo the $image 
  * @return string HTML IMG element for given image attachment 
  */
@@ -66,16 +65,37 @@ function wp_default_img( $attr )
 	// Sizes registered via add_image_size();
 	global $_wp_additional_image_sizes;
 
-	$defaults = array(
-		 'size'		=> 'medium'
-		,'classes'	=> false
-		,'alt'		=> false
-		,'title'	=> false
-		,'align'	=> 'none'
-		,'echo'		=> true 
-	);
+	// Error handling, if no default url
+	$error = ! isset( $attr['url'] ) 
+		? new WP_Error( 'url_missing', __( 'You need to specify an URl for the default img.' ), __FILE__ )
+		: false
+	;
+	if ( is_wp_error( $error ) )
+	{
+		// No error message for Guests or Subscribers
+		// Assuming that no one has activated caching plugins when debugging
+		// and not set WP_DEBUG to TRUE on a live site
+		if ( 
+			! is_user_logged_in()
+			AND ! current_user_can( 'edit_posts' ) 
+			AND ( ! defined( 'WP_DEBUG' ) OR ! WP_DEBUG )
+		)
+			return '';
 
-	$attr = wp_parse_args( $attr, $defaults );
+		return "{$error->get_error_message( 'no_attachment' )}: {$error->get_error_data()}";
+	}
+
+	$attr = wp_parse_args( $attr, array(
+		 'size'    => 'medium'
+		,'classes' => ''
+		,'alt'     => ''
+		,'title'   => ''
+		,'align'   => 'none'
+		,'echo'	   => true 
+	) );
+
+	// Sanitize
+	array_map( 'esc_attr', $attr );
 
 	if ( 'thumb' === $attr['size'] )
 		$attr['size'] = 'thumbnail';
@@ -84,12 +104,15 @@ function wp_default_img( $attr )
 	# behavoir in here, dependent on outcome of @link http://core.trac.wordpress.org/ticket/18947
 	if ( ! in_array( $attr['size'], array_keys( $_wp_additional_image_sizes ) ) )
 	{
-		$sizes						  = get_intermediate_image_sizes();
+		$sizes = get_intermediate_image_sizes();
+
 		// Get option - gladly autoloaded/can use wp_cache_get();
-		$size_data['width']	 = intval( get_option( "{$attr['size']}_size_w") );
-		$size_data['height']= intval( get_option( "{$attr['size']}_size_h") );
+		$size_data['width']  = intval( get_option( "{$attr['size']}_size_w") );
+		$size_data['height'] = intval( get_option( "{$attr['size']}_size_h") );
+
 		// Not sure how this will behave if cropped is false (autoloaded option not added)
-		$size_data['crop']	  = get_option( "{$attr['size']}_crop" ) ? get_option( "{$attr['size']}_crop" ) : false;
+		$crop_opt = get_option( "{$attr['size']}_crop" );
+		$size_data['crop']   = $crop_opt ? $crop_opt : false;
 	}
 	// Size array from global registered additional/custom sizes array
 	else 
@@ -98,43 +121,44 @@ function wp_default_img( $attr )
 	}
 
 	// Retrieve image width & height
-	$img_info		= @getimagesize( $attr['url'] );
+	$img_info  = @getimagesize( $attr['url'] );
 
-	// Calculate final dimensions - if "crop" was set to true during add_image_size(), the img will get ... cropped
-	$end_sizes		= image_resize_dimensions( $img_info[0], $img_info[1], $size_data['width'], $size_data['height'], $size_data['crop'] );
+	// Calculate final dimensions
+	// If "crop" was set to true during add_image_size(), the img will get ... cropped
+	$end_sizes = image_resize_dimensions( 
+		 $img_info[0]
+		,$img_info[1]
+		,$size_data['width']
+		,$size_data['height']
+		,$size_data['crop'] 
+	);
 
 	// defaults to px units - can't get changed, as applying units is not possible
-	$hwstring		= ' '.trim( image_hwstring( $end_sizes[4], $end_sizes[5] ) );
+	$hwstring  = ' '.trim( image_hwstring( $end_sizes[4], $end_sizes[5] ) );
 
-	// Attributes:
+	// >>>> Attributes:
 	// Not made required as users tend to do funky things (...and lock screen readers out)
-	$attr['alt'] = $attr['alt'] ? ' alt="'.esc_attr( $attr['alt'] ).'"' : '';
+	$attr['alt'] = ! emtpy( $attr['alt'] ) ? " alt='{$attr['alt']}'" : '';
 
 	if ( ! $attr['title'] )
 	{
 		$mime = explode( "/", $img_info['mime'] );
-		$attr['title'] = sprintf( __('default image of type: %1$s'), ucfirst( $mime[1] ) );
+		$mime = ucfirst( $mime[1] );
+		$attr['title'] = sprintf( __( 'default image of type: %1$s' ), $mime );
 	}
-	$attr['title'] = $attr['title'] ? ' title="'.esc_attr( $attr['title'] ).'"' : '';
-
-	$attr['classes'] = "wp-img-default ".esc_attr( $attr['classes'] ? $attr['classes'] : '' );
-	$attr['align'] = $attr['align'] ? "align".esc_attr( $attr['align'] ) : '';
-	$attr['size'] = "size-".esc_attr( $attr['size'] );
+	$attr['title']   = $attr['title'] ? " title='{$attr['title']}'" : '';
 
 	// Allow filtering of the default attributes
-	$attributes	 = apply_filters( 'wp_default_img_attr', $attr );
+	$attr    = apply_filters( 'wp_default_img_attr', $attr );
+	// <<<<
 
-	// Build class attribute, considering that maybe some attribute was unset via the filter
-	$classes  = ' class="';
-	$classes .= 'wp-img-default'.esc_attr( $attr['classes'] ? ' '.$attr['classes'] : '' );
-	$classes .= $attr['align'] ? ' '.esc_attr( $attr['align'] ) : '';
-	$classes .= $attr['size'] ? ' '.esc_attr( $attr['size'] ).'" ' : '" ';
+	$classes = " class='wp-img-default {$attr['classes']} size-{$attr['size']} {$attr['align']}'";
 
-	$url		= trim( $attr['url'] );
-	$image		= "<img src='{$url}'{$hwstring}{$classes}{$attr['alt']}{$attr['title']} />";
+	$url     = trim( $attr['url'] );
+	$image   = "<img src='{$url}'{$hwstring}{$classes}{$attr['alt']}{$attr['title']} />";
 
 	// Allow filtering of output
-	$image		= apply_filters( 'wp_default_img', $image );
+	$image   = apply_filters( 'wp_default_img', $image );
 
 	if ( ! $attr['echo'] )
 		return $image;
